f9b00b8e970f47a8244854f87930685a309a9c23
remyCESI <remy.vancamp@viacesi.fr>
2018-11-12 19:24:56 +0100

    ViewHomeEnd

---
+++ trip/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+
+        View home = new View();
+        home.init();
+    }
+}
---
+++ trip/src/Panel.java
@@ -0,0 +1,40 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Panel extends JPanel {
+
+
+
+
+        public void paintComponent(Graphics g){
+
+            try {
+
+                Image img = ImageIO.read(new File("BK.png"));
+
+
+
+                //Pour une image de fond
+
+                g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+            }
+
+    }
+
+
+
+    }
+
+
+
+
+
+
